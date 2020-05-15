e6c69cbc6bca8d709ec6379672dd0e065fc82310
mpaolodr <mpaolodr_1504@yahoo.com>
2020-05-14 22:42:40 -0400

    names.py solution added

--- names/binary_search_tree.py
+++ names/binary_search_tree.py
@@ -46,16 +46,13 @@ class BinarySearchTree:
                 else:
                     self.right = BinarySearchTree(value)
 
-    # Return True if the tree contains the value
-    # False if it does not
-
     def contains(self, target):
 
-        if self.value is target:
+        if self.value == target:
             return True
 
         else:
-            # goes left
+
             if target < self.value:
 
                 if self.left is not None:
@@ -63,16 +60,12 @@ class BinarySearchTree:
                 else:
                     return False
 
-            # target is less than current node's value
-            # goes right
             else:
 
                 if self.right is not None:
                     return self.right.contains(target)
                 else:
                     return False
-
-    # Return the maximum value found in the tree
 
     def get_max(self):
         if not self.value:
@@ -84,7 +77,6 @@ class BinarySearchTree:
             else:
                 return self.right.get_max()
 
-    # Call the function `fn` on the value of each node
     def for_each(self, fn):
         if not self.value:
             return
@@ -98,11 +90,6 @@ class BinarySearchTree:
             if self.right:
                 self.right.for_each(fn)
 
-    # Part 2 -----------------------
-
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
-
     def in_order_print(self, node):
         if node:
             self.in_order_print(node.left)
@@ -111,17 +98,11 @@ class BinarySearchTree:
 
             self.in_order_print(node.right)
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-
     def bft_print(self, node):
         h = self.height(node)
 
         for i in range(1, h+1):
             self.printGivenLevel(node, i)
-
-    #         Print the value of every node, starting with the given node,
-    #         in an iterative depth first traversal
 
     def dft_print(self, node):
         if node:
@@ -132,15 +113,6 @@ class BinarySearchTree:
 
             self.dft_print(node.right)
 
-        # if node:
-        #     print(node.value)
-        #     self.dft_print(node.right)
-        #     self.dft_print(node.left)
-
-        # Stretch Goals -------------------------
-        # Note: Research may be required
-
-        # Print Pre-order recursive DFT
     def pre_order_dft(self, node):
         if node:
             print(node.value)
@@ -182,57 +154,3 @@ class BinarySearchTree:
         elif level > 1:
             self.printGivenLevel(node.left, level-1)
             self.printGivenLevel(node.right, level-1)
-
-    # implement bft using queue
-    # def bft_print(self, node):
-    #     if node is None:
-    #         return
-
-    #     q = dll()
-
-    #     q.add_to_head(node)
-
-    #     while q.length > 0:
-    #         print(f"{q.head.value.value}")
-
-    #         removed_node = q.remove_from_head()
-
-    #         if removed_node.left is not None:
-    #             q.add_to_tail(removed_node.left)
-
-    #         if removed_node.right is not None:
-    #             q.add_to_tail(removed_node.right)
-
-
-def bft_print(self, node):
-    if node is None:
-        return
-
-    queueLL = Queue()
-
-    print(node.value)
-
-    queueLL.enqueue(node)
-    print(f"{queueLL.head}")
-
-    while queueLL.size > 0:
-        tracking_node = queueLL.dequeue()
-        if tracking_node.left:
-            queueLL.enqueue(tracking_node.left)
-        if tracking_node.right:
-            queueLL.enqueue(tracking_node.right)
-
-
-
-t = BinarySearchTree(1)
-
-t.insert(8)
-t.insert(5)
-t.insert(7)
-t.insert(6)
-t.insert(3)
-t.insert(4)
-t.insert(2)
-
-
-t.bft_print(t)
--- names/names.py
+++ names/names.py
@@ -1,4 +1,30 @@
 import time
+from binary_search_tree import BinarySearchTree as bst
+
+# start_time = time.time()
+
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# duplicates = []  # Return the list of duplicates in this data structure
+
+# # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# end_time = time.time()
+# print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print(f"runtime: {end_time - start_time} seconds")
+
+
+# WITH BST
 
 start_time = time.time()
 
@@ -12,15 +38,20 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# create a new tree from index 0 of names_1 array
+b = bst(names_1[0])
+
+# loop through names_1 array and add each elem in node
+for name in names_1:
+    b.insert(name)
+
+for name2 in names_2:
+    if b.contains(name2):
+        duplicates.append(name2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
